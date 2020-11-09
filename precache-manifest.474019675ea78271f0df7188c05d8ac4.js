21dba7c8f6aeaab0bd324d2ee75f44f04fbfef79
ANDRZEJ1988 <Molotkov2010>
2020-11-09 20:18:28 +0200

    Updates

---
+++ precache-manifest.474019675ea78271f0df7188c05d8ac4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f50031397379534602913c72f2deeb07",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "3ea29ddf8b4a96e8ac7a",
+    "url": "/movie-app/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "192be8e3467e15f23782",
+    "url": "/movie-app/static/css/main.19df527a.chunk.css"
+  },
+  {
+    "revision": "3ea29ddf8b4a96e8ac7a",
+    "url": "/movie-app/static/js/2.8cd767fa.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/movie-app/static/js/2.8cd767fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192be8e3467e15f23782",
+    "url": "/movie-app/static/js/main.67847eb6.chunk.js"
+  },
+  {
+    "revision": "424abb17f479c7475584",
+    "url": "/movie-app/static/js/runtime-main.1c26a08d.js"
+  },
+  {
+    "revision": "c1b9d05a0430b392ffff819bd6d67a47",
+    "url": "/movie-app/static/media/logo.c1b9d05a.png"
+  }
+]);
